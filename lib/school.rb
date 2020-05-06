ed7e9fa9cd3ade0fd685659ca26d4dbdb8dae168
Michael Evans <michaeljevans113@gmail.com>
2020-05-06 14:21:36 -0600

    Add methods to lib/school.rb and tests to test/school_test.rb

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,53 @@
+class School
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
 
+  def start_time
+    @start_time
+  end
+
+  def hours_in_school_day
+    @hours_in_school_day
+  end
+
+  def student_names
+    @student_names
+  end
+
+  def add_student_name(name)
+    student_names << name
+  end
+
+  def end_time
+   final = @start_time.to_i + @hours_in_school_day
+   end_time = "#{final}:00"
+  end
+
+  def is_full_time?
+    if @hours_in_school_day > 4
+      true
+    else
+      false
+    end
+  end
+
+  def standard_student_names
+    capitalized_names = []
+    student_names.each do |name|
+      capitalized_names << name.capitalize
+    end
+    student_names = capitalized_names
+  end
+
+  def convert_end_time_to_clock_time
+    final = @start_time.to_i + @hours_in_school_day
+    if final > 12
+      end_time = "#{final - 12}:00"
+    else
+      end_time = "#{final}:00"
+    end
+  end
+end
--- test/school_test.rb
+++ test/school_test.rb
@@ -5,28 +5,24 @@ require './lib/school'
 class SchoolTest < Minitest::Test
   #Iteration 1 Tests:
   def test_it_exists
-    skip
     school = School.new('9:00', 7)
 
     assert_instance_of School, school
   end
 
   def test_it_has_start_time
-    skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    skip
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
@@ -34,7 +30,6 @@ class SchoolTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_can_add_student_names
-    skip
     school = School.new('9:00', 7)
 
     school.add_student_name('Aurora')
@@ -45,11 +40,36 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_can_calculate_end_time
-    skip
     school1 = School.new('9:00', 7)
     school2 = School.new('9:00', 3)
 
     assert_equal '16:00', school1.end_time
     assert_equal '12:00', school2.end_time
+  end
+
+  def test_is_full_time
+    school1 = School.new('9:00', 7)
+    school2 = School.new('9:00', 3)
+
+    assert_equal true, school1.is_full_time?
+    assert_equal false, school2.is_full_time?
+  end
+
+  def test_are_names_standardized
+    school = School.new('9:00', 7)
+
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+
+    assert_equal ['Aurora', 'Tim', 'Megan'], school.standard_student_names
+  end
+
+  def test_is_end_time_converted_to_clock_time
+    school1 = School.new('9:00', 7)
+    school2 = School.new('9:00', 3)
+
+    assert_equal '4:00', school1.convert_end_time_to_clock_time
+    assert_equal '12:00', school2.convert_end_time_to_clock_time
   end
 end
